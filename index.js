83b6403a503bd2ba3c1137323040e84d5dc49569
Gilblasse <gilblasse@gmail.com>
2020-02-29 15:11:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(array, func) {
+  let result = [];
+  array.forEach(num => result.push(func(num)));
+  return result;
+}
+
+function reduce(array, func, startingPoint) {
+  let start = startingPoint || array[0];
+  array.forEach(elmt => (start = func(elmt, start)));
+  return typeof start == "number"
+    ? !!startingPoint
+      ? start
+      : start - array[0]
+    : start;
+}
